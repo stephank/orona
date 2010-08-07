c48abe2fe3c1c3cbb45a7fd78c8e88b58d480605
Stéphan Kochen <stephan@kochen.nl>
2010-08-07 21:27:20 +0200

    Adding copyright notices to source files.

--- util/bmap2ascii.py
+++ util/bmap2ascii.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# Orona, © 2010 Stéphan Kochen
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
 import sys, struct, copy
 
 fo = open(sys.argv[1], 'r')
